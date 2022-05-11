a2efb7cef6fea3588581dcb9f51444673e318621
mrm65 <minan.65@hotmail.com>
2022-05-11 16:20:57 +0300

    update

--- Services/Order/Services.Order.API/Program.cs
+++ Services/Order/Services.Order.API/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Order.Infrastructure;
 
 namespace Services.Order.API
 {
@@ -13,7 +16,15 @@ namespace Services.Order.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var serviceProvider = scope.ServiceProvider;
+                var orderDbContext = serviceProvider.GetRequiredService<OrderDbContext>();
+                orderDbContext.Database.Migrate();
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
